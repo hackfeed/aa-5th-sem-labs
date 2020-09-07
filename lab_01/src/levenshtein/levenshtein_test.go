0beea25f738227ded2164d9a2dc832f51f66deb6
Sergey Kononenko <hackfeed@yandex.ru>
2020-09-07 12:44:00 +0300

    Add benchmarks and styling

---
+++ lab_01/src/levenshtein/levenshtein_test.go
@@ -0,0 +1,225 @@
+package levenshtein
+
+import (
+	"testing"
+)
+
+// Recursive method benchmarks.
+
+func BenchmarkRecursiveLen5(b *testing.B) {
+	fWord := "about"
+	sWord := "above"
+
+	for i := 0; i < b.N; i++ {
+		Recursive(fWord, sWord)
+	}
+}
+
+func BenchmarkRecursiveLen10(b *testing.B) {
+	fWord := "abbanition"
+	sWord := "abaptiston"
+
+	for i := 0; i < b.N; i++ {
+		Recursive(fWord, sWord)
+	}
+}
+
+// func BenchmarkRecursiveLen15(b *testing.B) {
+// 	fWord := "characteristics"
+// 	sWord := "recommendations"
+
+// 	for i := 0; i < b.N; i++ {
+// 		Recursive(fWord, sWord)
+// 	}
+// }
+
+// RecursiveMatrix method becnhmarks.
+
+func BenchmarkRecursiveMatrixLen10(b *testing.B) {
+	fWord := "abbanition"
+	sWord := "abaptiston"
+
+	for i := 0; i < b.N; i++ {
+		RecursiveMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkRecursiveMatrixLen20(b *testing.B) {
+	fWord := "abdominohysterectomy"
+	sWord := "acetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		RecursiveMatrix(fWord, sWord)
+	}
+}
+func BenchmarkRecursiveMatrixLen30(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrile"
+	sWord := "abdominalexternalobliquemuscle"
+
+	for i := 0; i < b.N; i++ {
+		RecursiveMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkRecursiveMatrixLen50(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		RecursiveMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkRecursiveMatrixLen100(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		RecursiveMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkRecursiveMatrixLen200(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		RecursiveMatrix(fWord, sWord)
+	}
+}
+
+// IterativeMatrix method benchmarks.
+
+func BenchmarkIterativeMatrixLen10(b *testing.B) {
+	fWord := "abbanition"
+	sWord := "abaptiston"
+
+	for i := 0; i < b.N; i++ {
+		IterativeMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkIterativeMatrixLen20(b *testing.B) {
+	fWord := "abdominohysterectomy"
+	sWord := "acetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		IterativeMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkIterativeMatrixLen30(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrile"
+	sWord := "abdominalexternalobliquemuscle"
+
+	for i := 0; i < b.N; i++ {
+		IterativeMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkIterativeMatrixLen50(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		IterativeMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkIterativeMatrixLen100(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		IterativeMatrix(fWord, sWord)
+	}
+}
+
+func BenchmarkIterativeMatrixLen200(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		IterativeMatrix(fWord, sWord)
+	}
+}
+
+// DamerauLevenshtein method benchmarks.
+
+func BenchmarkDamerauLevenshteinLen10(b *testing.B) {
+	fWord := "abbanition"
+	sWord := "abaptiston"
+
+	for i := 0; i < b.N; i++ {
+		DamerauLevenshtein(fWord, sWord)
+	}
+}
+
+func BenchmarkDamerauLevenshteinLen20(b *testing.B) {
+	fWord := "abdominohysterectomy"
+	sWord := "acetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		DamerauLevenshtein(fWord, sWord)
+	}
+}
+
+func BenchmarkDamerauLevenshteinLen30(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrile"
+	sWord := "abdominalexternalobliquemuscle"
+
+	for i := 0; i < b.N; i++ {
+		DamerauLevenshtein(fWord, sWord)
+	}
+}
+
+func BenchmarkDamerauLevenshteinLen50(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		DamerauLevenshtein(fWord, sWord)
+	}
+}
+
+func BenchmarkDamerauLevenshteinLen100(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		DamerauLevenshtein(fWord, sWord)
+	}
+}
+
+func BenchmarkDamerauLevenshteinLen200(b *testing.B) {
+	fWord := "chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy" +
+		"chlorobenzylidenemalononitrileabdominohysterectomy"
+	sWord := "abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase" +
+		"abdominalexternalobliquemuscleacetylcholinesterase"
+
+	for i := 0; i < b.N; i++ {
+		DamerauLevenshtein(fWord, sWord)
+	}
+}
--- lab_01/src/main.go
+++ lab_01/src/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/logrusorgru/aurora"
+
 	"./levenshtein"
 )
 
 func main() {
+	fmt.Println(aurora.Magenta("Levenshtein distance finder\n"))
+
 	fmt.Printf("Input first word : ")
 	fWord := levenshtein.ReadWord()
 	fmt.Printf("Input second word: ")
@@ -15,27 +19,27 @@ func main() {
 	fmt.Println()
 
 	distRec := levenshtein.Recursive(fWord, sWord)
-	fmt.Printf("Recursive method without matrix filling: \n")
-	fmt.Printf("Distance: %d\n\n", distRec)
+	fmt.Println(aurora.Yellow("Recursive method without matrix filling: "))
+	fmt.Printf("Distance: %d\n\n", aurora.Green(distRec))
 
 	fmt.Println()
 
 	distRM, matRM := levenshtein.RecursiveMatrix(fWord, sWord)
-	fmt.Printf("Recursive method with matrix filling: \n")
+	fmt.Println(aurora.Yellow("Recursive method with matrix filling: "))
 	matRM.PrintMatrix()
-	fmt.Printf("Distance: %d\n\n", distRM)
+	fmt.Printf("Distance: %d\n\n", aurora.Green(distRM))
 
 	fmt.Println()
 
 	distIt, matIt := levenshtein.IterativeMatrix(fWord, sWord)
-	fmt.Printf("Iterative method with matrix filling: \n")
+	fmt.Println(aurora.Yellow("Iterative method with matrix filling: \n"))
 	matIt.PrintMatrix()
-	fmt.Printf("Distance: %d\n\n", distIt)
+	fmt.Printf("Distance: %d\n\n", aurora.Green(distIt))
 
 	fmt.Println()
 
 	distDL, matDL := levenshtein.DamerauLevenshtein(fWord, sWord)
-	fmt.Printf("Damerau-Levenshtein method with matrix filling: \n")
+	fmt.Println(aurora.Yellow("Damerau-Levenshtein method with matrix filling: \n"))
 	matDL.PrintMatrix()
-	fmt.Printf("Distance: %d\n\n", distDL)
+	fmt.Printf("Distance: %d\n\n", aurora.Green(distDL))
 }
